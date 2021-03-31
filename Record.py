91e3dd2377a28535450deb5ab34654395ca40ac7
ycheng <36308352+ycheng22@users.noreply.github.com>
2021-03-31 18:18:35 -0500

    Add files via upload

---
+++ Record.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+# __author__ = ''
+
+'''
+快捷键
+段注释 ctrl+/
+联想补全 ctrl+shift+enter
+.rstrip() 删除换行符
+'''
---
+++ hello_word.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+# __author__ = ''
+
+
+#coding=utf-8
+
+#print('hello world')
+
+# print('\t python')
+# print(message.title())
+# message='hello'
+# print(message)
+
+# name=input('tell me')
+# print('hello'+name+'!')
+
+pets=['dog','cat','dog','cat']
+print(pets)
+while 'cat' in pets:
+	pets.remove('cat')
+print(pets)
+
